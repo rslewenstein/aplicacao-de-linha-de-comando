0502b5fcda99a2a6fa936adb2836df52cacdcf3a
rslewenstein <rafasoaresjf@gmail.com>
2021-07-29 23:06:54 -0300

    Comando buscar DNS

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 * Para instalar o pacote:
 go get github.com/urfave/cli
+
+## Executando o projeto:
+
+* Para buscar o IP
+go run main.go ip --host amazon.com.br
+
+* Para buscar os servidores DNS
+go run main.go servidores --host amazon.com.br
--- app/app.go
+++ app/app.go
@@ -13,17 +13,25 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na Internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
 			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome de servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
 		},
 	}
 
@@ -40,5 +48,18 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host) // Name server
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }
