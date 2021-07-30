a6f4ecfa6052848406f6b450f3db5944e8ad4110
rslewenstein <rafasoaresjf@gmail.com>
2021-07-29 22:56:20 -0300

    primeira fucao

--- app/app.go
+++ app/app.go
@@ -1,11 +1,44 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na Internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPs de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
