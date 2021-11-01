a9fa04d3c8a9c8c828521b7364a09435320332d7
rslewenstein <rafasoaresjf@gmail.com>
2021-11-01 18:07:50 -0300

    Ferramentas e dicas para os testes

--- Testes Automatizados/1 - Introdução/enderecos/enderecos_test.go
+++ Testes Automatizados/1 - Introdução/enderecos/enderecos_test.go
@@ -17,7 +17,7 @@ func TestTipoDeEndereco(t *testing.T) {
 		{"RUA DOS BOBOS", "Rua"},
 		{"AVENIDA REBOUÇAS", "Avenida"},
 		{"", "Tipo Inválido"},
-		{"Avenida das Bromélias", "Estrada"}, // teste de erro
+		//{"Avenida das Bromélias", "Estrada"}, // teste de erro
 	}
 
 	for _, cenario := range cenariosDeTeste {
@@ -35,4 +35,12 @@ func TestTipoDeEndereco(t *testing.T) {
 // para realizar os testes, é obrigatório:
 // O nome do arquivo deve ter o _test
 // O nome da função deve começar com "Test"
-// para executar, digite: go test
+
+// para executar, digite: go test (+ parametros)
+
+// Parametros:
+// (-v) = Mais detalhes
+// (--cover) = exibe qual a porcentagem de cobertura dos testes
+// (--coverprofile nome_arquivo.txt) = gera um arquivo com as informações
+// (go tool cover --func=nome_arquivo.txt) = lê as informações do arquivo gerado
+// (go tool cover --html=nome_arquivo.txt) = exibe as informações das linhas que não estão cobertas
